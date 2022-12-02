a7627486b0687fb68221bb8158bbcb9de79c7393
Arya Sena Aji <aryasa1997@gmail.com>
2022-12-02 13:39:23 +0700

    Minor add

--- App.js
+++ App.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+
 app.get('/', (req,res)=>{
     res.send('Hello World');
 })
