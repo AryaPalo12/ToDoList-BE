a86cd81b57bfd42aff260c92dd534cd02ebf3174
Arya Sena Aji <aryasa1997@gmail.com>
2022-12-03 23:19:01 +0700

    test

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -6,4 +6,9 @@ const { validate } = require("../middleware/validation");
 
 userRouter.post('/registration',validate, userControllers.createUser);
 
+userRouter.options((req,res)=>{
+    return res.status(200).send('OK');
+})
+
+
 module.exports = userRouter;
