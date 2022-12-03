6047ef911f19db7c1173eb19a1c6ec17129998d5
Arya Sena Aji <aryasa1997@gmail.com>
2022-12-03 23:05:04 +0700

    test

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const micro = require("micro-cors");
 const express = require('express');
 const app = express();
 const authRouter = require("./src/auth/auth.router");
@@ -7,6 +8,14 @@ const listRouter = require("./src/list/list.router");
 const port = process.env.PORT || 4000;
 const cors = require('cors')
 
+
+
+
+
+const cors = micro();
+
+export default cors(MyApi);
+
 app.use(cors());
 
 app.use(express.json());
@@ -14,6 +23,12 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('Hello World');
 })
+
+function MyApi(req, res) {
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+  }
 
 app.use(userRouter);
 app.use(listRouter);
